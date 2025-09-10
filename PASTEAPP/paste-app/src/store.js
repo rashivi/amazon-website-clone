cc08ad6f7769cc2f0fb180d8371f8f0bd48c014b
rashivi <vishwakarmarashi365@gmail.com>
2025-09-10 16:58:17 +0530

    Delete PASTEAPP directory

--- PASTEAPP/paste-app/src/store.js
+++
@@ -1,9 +0,0 @@
-
-import { configureStore } from '@reduxjs/toolkit'
-import pasteReducer from './redux/pasteSlice'
-
-export default configureStore({
-  reducer: {
-    paste: pasteReducer,
-  },
-})
